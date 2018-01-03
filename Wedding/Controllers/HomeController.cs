347f7e27d17e6a0f98c6cb8e0d17e660a7f6d7ce
Tiago <tiagoduro13@gmail.com>
2018-01-03 22:26:14 +0000

    Alpha version

--- Wedding/Controllers/HomeController.cs
+++ Wedding/Controllers/HomeController.cs
@@ -13,16 +13,14 @@ namespace Wedding.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Place()
         {
-            ViewBag.Message = "Your application description page.";
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Answer()
         {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
